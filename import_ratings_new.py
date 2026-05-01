4bfd6604084f0acd9f7ae929fe8a705845487fca
John <darkcodeninja@gmail.com>
2026-04-30 21:45:47 -0600

    Plex Albums and Songs now rated

--- import_ratings_new.py
+++ import_ratings_new.py
@@ -1,13 +1,10 @@
 #todo:
 #print behavior to log file and console for automatic imports.
 
-from datetime import datetime
-import sys
 import os
 import time
 from plexapi.server import PlexServer
-from plexapi.exceptions import BadRequest
-#from libpytunes import Library
+from libpytunes import Library
 import tomllib
 
 def yn_prompt(question, default, yesResponse, noResponse):
@@ -56,7 +53,7 @@ def progress_bar(completed, total, message):
 
 
 if __name__ == '__main__':
-    #open config file and save it to local variable config
+    #open config file and save it to local variable
     config = {}
     with open("config.toml", "rb") as configFile:
         config = tomllib.load(configFile)
@@ -64,7 +61,6 @@ if __name__ == '__main__':
     #create local variables for config values
     plexUrl = config["PlexInfo"]["url"]
     plexServerToken = config["PlexInfo"]["serverToken"]
-    plexServerName = config["PlexInfo"]["serverName"]
     plexLibraryName = config["PlexInfo"]["libraryName"]
 
     iTunesLibraryFile = config["iTunesInfo"]["libraryFile"]
@@ -95,9 +91,108 @@ if __name__ == '__main__':
                                                   "Skipping existing Plex album ratings.")
     
     #load library from iTunes
-    #iTunesLibrary = Library(iTunesLibraryFile)
+    iTunesLibrary = Library(iTunesLibraryFile)
 
-    for i in range(101):
-        time.sleep(0.05)
-        progress_bar(i, 100, "test progress bar")
-    print("\ndone!")
+    #dictionary for indexed iTunes songs. key structure: [artist][album][track] -> song data
+    iTunesSongIndex = {}
+
+    #dictionary for indexed iTunes albums. key structure [album name] -> data from first song on album.
+    iTunesAlbumIndex = {}
+
+    #index will track the progress of the loop.
+    print("Indexing iTunes Songs and Albums...")
+    index = 0
+    songsWithRatings = 0
+    albumsWithRatings = 0
+    for x, song in iTunesLibrary.songs.items():
+        #see if song has a valid rating value
+
+        if song and song.rating and song.rating > 10 and (not song.rating_computed or useComputedSongRatings):
+            #convert name, album, and artist data to string from XML.
+            songName = str(song.name)
+            songAlbum = str(song.album)
+            songAlbumArtist = str(song.album_artist)
+            
+            #plex uses a rating system between 1 and 10, and iTunes uses between 10 and 100.
+            songRating = song.rating / 10
+
+            #add artist if it doesn't already exist:
+            if songAlbumArtist not in iTunesSongIndex:
+                iTunesSongIndex[songAlbumArtist] = {}
+            
+            #add album to index if it doesn't already exist.
+            if songAlbum not in iTunesSongIndex[songAlbumArtist]:
+                iTunesSongIndex[songAlbumArtist][songAlbum] = {}
+
+            #add rating to song index, and increment songsWithRatings for final count
+            iTunesSongIndex[songAlbumArtist][songAlbum][songName] = songRating
+            songsWithRatings += 1
+        
+        #see if song's album has a valid rating value
+        if song and song.album_rating and song.album_rating > 10 and (not song.album_rating_computed or useComputedAlbumRatings):
+            artistName = str(song.album_artist)
+            albumName = str(song.album)
+
+            #only add album data if it doesn't exist
+            if song.album_artist not in iTunesAlbumIndex:
+                artistName = str(song.album_artist)
+                iTunesAlbumIndex[artistName] = {}
+
+            if song.album not in iTunesAlbumIndex[artistName]:
+                albumRating = song.album_rating / 10
+                iTunesAlbumIndex[artistName][albumName] = albumRating
+                albumsWithRatings += 1
+
+        #update progress bar with song/album being processed
+        progress_bar(index, len(iTunesLibrary.songs.items())-1, f"Indexing iTunes library...")
+        index += 1
+
+    print(f"\niTunes songs with ratings: {songsWithRatings}")
+    print(f"iTunes albums with ratings: {albumsWithRatings}")
+
+    #connect to plex server and get music library
+    print("\nConnecting to Plex server...")
+    plexServer = PlexServer(plexUrl, plexServerToken)
+    musicLibrary = plexServer.library.section(plexLibraryName)
+
+    #load music library into plexSongs variable
+    print(f"Loading Plex music library '{plexLibraryName}'...")
+    plexSongs = musicLibrary.searchTracks()
+    plexSongsCount = len(plexSongs)
+    print(f"Number of songs on Plex: {plexSongsCount}")
+    time.sleep(1)
+    
+    index = 0
+
+    #temp!
+    matchingSongs = 0
+    matchingAlbums = 0
+    for song in plexSongs:
+        songArtist = str(song.artist().title)
+        songAlbum = str(song.album().title)
+        songName = str(song.title)
+
+        #check to see if there is a match for the song in the iTunes index
+        if songArtist in iTunesSongIndex:
+            if songAlbum in iTunesSongIndex[songArtist]:
+                if songName in iTunesSongIndex[songArtist][songAlbum]:
+                    if song.userRating is None or overwriteExistingSongRatings:
+                        songRating = iTunesSongIndex[songArtist][songAlbum][songName]
+                        matchingSongs += 1
+                        song.rate(songRating)
+
+        #check to see if there is a match for the album in the iTunes index
+        if songArtist in iTunesAlbumIndex:
+            if songAlbum in iTunesAlbumIndex[songArtist]:
+                album = song.album()
+                albumRating = iTunesAlbumIndex[songArtist][songAlbum]
+                if album.userRating != albumRating and (album.userRating is None or overwriteExistingAlbumRatings):
+                    matchingAlbums += 1
+                    album.rate(albumRating)
+
+        progress_bar(index, len(plexSongs)-1, f"Applying ratings to Plex library...")
+        index += 1
+    
+    print(f"\nPlex tracks rated: {matchingSongs}")
+    print(f"Plex albums rated: {matchingAlbums}")
+    print("Rating import complete.")
